309c6ed02fc138bb8b72d061d3234d99aef0ad83
IamBorn <piyoungkoon@gmail.com>
2024-04-25 14:49:08 +0700

    Move template.cpp to folder template

---
+++ templates/template.cpp
@@ -0,0 +1,21 @@
+// source : 
+// status : incomplete
+// date : 
+// problem tags : 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sz(v) ((int)(v).size)
+#define all(v) (v).begin(), (v).end()
+
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  
+  return 0;
+}
